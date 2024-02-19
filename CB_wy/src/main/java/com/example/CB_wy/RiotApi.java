7f534fe17ce39b92125bcf2d64124501aac8b148
wooDragonHub <yiwooy5@gmail.com>
2024-02-19 21:03:47 +0900

    [riot-api] api key 추가 및 보완 강화

--- CB_wy/src/main/java/com/example/CB_wy/RiotApi.java
+++ CB_wy/src/main/java/com/example/CB_wy/RiotApi.java
@@ -2,11 +2,15 @@ package com.example.CB_wy;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class RiotApi {
+    @Value("${RIOT-KEY}")
+    private String RIOT_KEY;
+
     @GetMapping("/livechk")
     public String live_chk(){
-        return "live";
+        return RIOT_KEY;
     }
 }
